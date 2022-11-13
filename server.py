c627502a4de097fc6a4204bb0588db9e4f055ff4
Valerie Wong <wongvalerie45@gmail.com>
2022-11-13 00:18:40 -0500

    testing without venv folder

--- server.py
+++ server.py
@@ -18,5 +18,7 @@ def login_page():
 def home_page():
     return render_template('home.html')
 
+#test
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True) #keep as 8080?
