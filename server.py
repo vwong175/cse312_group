46bea5fe91f2b22779f349415f4adefe9291bb67
Anh Tran <kyanhtra@buffalo.edu>
2022-11-16 01:45:00 -0500

    add barebone of leaderboard & modify app route

--- server.py
+++ server.py
@@ -11,12 +11,33 @@ db = client['r_p_s']    #Select the database
 users = db['users']
 
 @app.route('/', methods=["GET"])
+def redirect_page():
+    return redirect(url_for('register_page'))
+
+@app.route('/login', methods=["GET"])
 def login_page():
+    return render_template('login.html')
+
+@app.route('/register', methods=["GET"]) 
+def register_page():
     return render_template('register.html')
 
 @app.route('/home', methods=["GET"])
 def home_page():
     return render_template('home.html')
+
+@app.route('/leaderboard', methods=["GET"])
+def leaderboard_page():
+    return render_template('leaderboard.html')
+
+class User():
+    def __init__(self):
+        self.username = 'test'
+        self.wins = 1
+        self.played = 1
+@app.route('/profile', methods=["GET"])
+def profile_page():
+    return render_template('profile.html', user= User(), rank=1)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True) #keep as 8080?
