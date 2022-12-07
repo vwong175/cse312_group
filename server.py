b65bcbdfa60865a5e85ce4f3cd4a6a02fb66e1de
Ronald Chen <70644931+mraddtab@users.noreply.github.com>
2022-12-07 16:54:22 -0500

    render edit username
    render edit username

--- server.py
+++ server.py
@@ -48,6 +48,7 @@ def signout_page():
 # a user's profile page
 @app.route("/profile/")
 def profileCheck():
+<<<<<<< Updated upstream
   if session.get("userid") == None:
     return jsonify({"failed": "Login first."}), 401
   return redirect('/profile/'+session.get("userid"))
@@ -60,6 +61,40 @@ def profile_page(userid):
         return jsonify({"failed": "Login first."}), 401
     user = users.find_one({"_id": userid})
     return render_template('profile.html', user=user)
+=======
+  if session.get("username") == None:
+    return jsonify({"failed": "Login first to view profiles."}), 401
+
+  return redirect('/profile/'+session.get("username"))
+
+#TODO: A user should be able to see another user's information, just not edit it
+# any user's page
+# change to view all users by username but edit only with session login
+@app.route('/profile/<string:username>', methods=['GET'])
+def profile_page(username):
+    user = users.find_one({"username": username})
+    if user:
+        editUsernameForm = editUserForm()
+        return render_template('profile.html', form=editUsernameForm, user=user, username=session.get('username') , rank="Not on list")
+    else:
+        return jsonify({"failed": "User can not be found"}), 401
+
+@app.route('/profile/<string:username>', methods=['POST'])
+def edit_username(username):
+    if session.get("username") != username:
+        return jsonify({"failed": "In order to change this account's username, please login."}), 401
+
+    newUsername = request.form['newUsername']
+
+    is_avialable_name = users.find_one({"username": session.get('username')}) == None
+    if is_avialable_name == False:
+        flash("Username address already in use")
+        return redirect('/profile/'+session.get("username"))
+ 
+    users.update_one({"username": session.get("username")}, {"$set": {'username': newUsername}})
+    session["username"] = newUsername
+    return render_template('profile.html', username=newUsername)
+>>>>>>> Stashed changes
 
 # leaderboard page
 @app.route('/leaderboard/')
